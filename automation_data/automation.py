1435074a01ef80c0fc4161823d087c0f54b5ac3a
bonfaceonyango <bonfaceb21@gmail.com>
2022-09-03 12:51:22 +0300

    updated automated file

---
+++ automation_data/automation.py
@@ -0,0 +1,11 @@
+import getopt, sys, subprocess,os
+
+#make file executable
+subprocess.Popen("chmod +x ../script/bam2fa.sh", shell=True)
+# Export conda Environment
+#subprocess("conda env create -f  ml_ancestry_conda.yml",shell=True) # 
+
+# Activate conda environment 
+subprocess("conda activate ml_ancestry_conda",shell=True) # Activate your conda environment 
+# Generate concessus sequence from bam file
+subprocess.Popen(["../script/bam2fa.sh"]) # bash script has shebang, hence avoid shell =True
