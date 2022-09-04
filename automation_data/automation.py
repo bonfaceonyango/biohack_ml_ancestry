4ab08ab6eb7c335929f4682616d215a54ee632be
bonfaceonyango <bonfaceb21@gmail.com>
2022-09-04 14:33:46 +0300

    updated script

--- automation_data/automation.py
+++ automation_data/automation.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python
 import subprocess
+subprocess.run("chmod +x ../script/bam2fa.sh", shell=True)
+# Run the bash script
+subprocess.run("../script/bam2fa.sh", shell=True)
 
-#make file executable
-subprocess.Popen("chmod +x ../script/bam2fa.sh", shell=True)
-# Export conda Environment
-#subprocess("conda env create -f  ml_ancestry_conda.yml",shell=True) # 
 
-# Activate conda environment 
-subprocess(["conda activate ml_phylogeny"],shell=True) # Activate your conda environment 
-# Generate concessus sequence from bam file
-subprocess.Popen(["../script/bam2fa.sh"]) # bash script has shebang, hence avoid shell =True
 
+
+
+#Docker refs
+#https://medium.com/@chadlagore/conda-environments-with-docker-82cdc9d25754
