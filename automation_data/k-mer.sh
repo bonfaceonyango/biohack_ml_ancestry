9d2cf949f2a159d3be3b0be1492244efce6fb720
bonfaceonyango <bonfaceb21@gmail.com>
2022-09-06 23:03:30 +0300

    generate k-mer

---
+++ automation_data/k-mer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+cd "consensus/"
+for k in 27
+do
+   kmer_prof_dir="${k}_mers/"
+
+   mkdir -p ${kmer_prof_dir}
+
+   #example shown for files of AFR population
+   for sample in `ls *.fa`
+   do
+       echo "starting generating kmers for $sample"
+       sample_file="${sample}"
+       jf_file="${kmer_prof_dir}${sample}_${k}_mers.jf"
+       fa_file="${kmer_prof_dir}${sample}_${k}_mers.fa"
+
+       jellyfish count ${sample_file} -m ${k} -s 100M -t 50 -L 10 -C --disk -o ${jf_file}
+
+       jellyfish dump ${jf_file} > ${fa_file} -c
+
+       rm ${jf_file}
+       echo "completed generating kmers for $sample"
+   done
+   done
+cd "../"
