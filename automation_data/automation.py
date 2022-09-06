ce85bc88ba90fe1faf569594bd93734f87325ca0
bonfaceonyango <bonfaceb21@gmail.com>
2022-09-06 23:00:03 +0300

    updated

--- automation_data/automation.py
+++ automation_data/automation.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 import subprocess
+
 subprocess.Popen("chmod +x ./bam2fa.sh", shell=True)
 # Run the bash script
 subprocess.run("./bam2fa.sh", shell=True)
 #Give executable permission to kmer script
+
 subprocess.Popen("chmod +x ./k-mer.sh", shell=True)
 #Execute the k-mer script
 subprocess.Popen("./k-mer.sh", shell=True)
 
+#subprocess.Popen("chmod +x ./frequent_kmers.sh", shell=True)
+#subprocess.Popen("./frequent_kmers.sh", shell=True)
 
 
 
